04ff62ceab41a93f6f8b660e5f6aae170c3b1b68
Maryan23 <maryann.mwikali@student.moringaschool.com>
2021-11-12 15:38:08 +0300

    Add and style navbar

--- config.py
+++ config.py
@@ -4,19 +4,22 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    pass  
+    DEBUG = True
 
 class ProdConfig(Config):
     '''
     Production configuration child class
     '''
     pass
+    DEBUG = True
 
 class DevConfig(Config):
     '''
     Development configuration child class
     '''
     pass
+    DEBUG = True
 
 config_options = {
    "production" :ProdConfig,
