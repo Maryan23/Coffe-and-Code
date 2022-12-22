b65e83e1c83dad2a0fd70c915eb8878893704e73
Maryann Mwikali <mwikali119@gmail.com>
2022-12-22 22:24:12 +0300

    Deploy to railways

--- migrations/versions/efc6ad3ef566_.py
+++ migrations/versions/efc6ad3ef566_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: f3407299475a
+Revision ID: efc6ad3ef566
 Revises: 
-Create Date: 2022-12-22 10:12:14.341054
+Create Date: 2022-12-22 22:08:33.579777
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'f3407299475a'
+revision = 'efc6ad3ef566'
 down_revision = None
 branch_labels = None
 depends_on = None
